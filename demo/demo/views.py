9a9935de9bfbf2b28630310d58d82fdea3c68bc5
zjm <jinminghenpiao@163.com>
2019-11-01 20:51:46 +0800

    这是一个小文件

--- demo/demo/views.py
+++ demo/demo/views.py
@@ -6,4 +6,4 @@ def index(zjm):
 def demo(request,id):
     return HttpResponse('hello')
 def getDay(request,year,month,day):
-    return HttpResponse("%s/%s/%s是K%s的第63天"%(year,month,day,year))
+    return HttpResponse("%s/%s/%s是%s年的第63天"%(year,month,day,year))
